bfc6213872727b090c1bd40c880f402beafdd8ce
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-08-13 14:24:07 +0300

    styling tips

--- Unicounselor/src/app/pages/tips/tips.page.ts
+++ Unicounselor/src/app/pages/tips/tips.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Tip, TipsService } from 'src/app/apis/tips.service';
+import { TipsService } from 'src/app/apis/tips.service';
 
 @Component({
   selector: 'app-tips',
