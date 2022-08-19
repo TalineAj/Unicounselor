c2e8ba12ad2ef4a8cf11a62f1575f679f5c531c8
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-08-19 21:17:32 +0300

    displaying retrieved user info in myinformation

--- Unicounselor/src/app/pages/myinformation/myinformation.page.ts
+++ Unicounselor/src/app/pages/myinformation/myinformation.page.ts
@@ -8,13 +8,29 @@ import { AuthService } from 'src/app/apis/auth.service';
 })
 export class MyinformationPage implements OnInit {
   id: any;
-
+  user: any;
+  firstname= null;
+  lastname= null;
+  dateofbirth= null;
+  gender= null;
+  email= null;
   constructor(private authService: AuthService) { }
   ngOnInit() {
      this.id =  this.authService.getCurrentUserId();
-    this.authService.getUserById(this.id).subscribe(res =>{
-console.log(res);
-    });
+if(this.id){
+  //there is a signed in user
+  this.authService.getUserById(this.id).subscribe(res =>{
+    this.user = res;
+    console.log(this.user);
+    this.firstname = this.user.firstname;
+    this.lastname = this.user.lastname;
+    this.dateofbirth = this.user.dateofbirth;
+    this.gender = this.user.gender;
+    this.email = this.user.email;
+  });
+}else{
+ console.log('no user signed in');
+}
   }
 
 }
