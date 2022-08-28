30cc271f8b2c70ffb6530497c35d9e9ae45b18c7
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-08-29 01:42:38 +0300

    adding js to homec

--- Unicounselor/src/app/pages/homec/homec.page.ts
+++ Unicounselor/src/app/pages/homec/homec.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/apis/auth.service';
 
 @Component({
   selector: 'app-homec',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homec.page.scss'],
 })
 export class HomecPage implements OnInit {
+  today = new Date().toLocaleDateString();
+  id: any;
+  user: any;
+  firstname: any;
+  name: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.id =  this.authService.getCurrentUserId();
+  if(this.id){
+   //there is a signed in user
+   this.authService.getUserById(this.id).subscribe(res =>{
+     this.user = res;
+     this.firstname = this.user.firstname;
+     this.name = this.user.firstname + ' ' + this.user.lastname;
+   });
+  }else{
+  console.log('no user signed in');
+  }
   }
 
 }
