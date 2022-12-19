6bb4dae4afaf834bb8da405857bf857f2a038303
unknown <taline.aboujaoude@lau.edu>
2022-12-19 20:41:58 +0200

    Student rating counselor , reviewmodal -Tidying and Testing

--- Unicounselor/src/app/pages/reviewsmodal/reviewsmodal.page.ts
+++ Unicounselor/src/app/pages/reviewsmodal/reviewsmodal.page.ts
@@ -8,7 +8,7 @@ import {
 } from '@ionic/angular';
 import { ReviewsService, Review } from 'src/app/apis/reviews.service';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import {Firestore} from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-reviewsmodal',
   templateUrl: './reviewsmodal.page.html',
@@ -19,62 +19,70 @@ export class ReviewsmodalPage implements OnInit {
   lastname: any;
   review: Review;
   studentname: any;
-  noreviews =0;
+  noreviews = 0;
   fetchedreviews = [];
 
-  constructor( private firestore: Firestore,
-    private navParams: NavParams, private modalController: ModalController, private alertController: AlertController,
+  constructor(
+    private firestore: Firestore,
+    private navParams: NavParams,
+    private modalController: ModalController,
+    private alertController: AlertController,
     private loadingController: LoadingController,
-    private toastController: ToastController, private reviewsService: ReviewsService) { }
+    private toastController: ToastController,
+    private reviewsService: ReviewsService
+  ) {}
 
-    async ngOnInit() {
+  async ngOnInit() {
     //getting props
     this.firstname = this.navParams.get('fname');
     this.lastname = this.navParams.get('lname');
     this.studentname = this.navParams.get('student');
 
     //retreiving the reviews of selected counselor
-    const counselorRef =collection(this.firestore,'Reviews');
-    const q = query(counselorRef, where('counselor', '==', this.firstname + ' '+ this.lastname));
+    const counselorRef = collection(this.firestore, 'Reviews');
+    const q = query(
+      counselorRef,
+      where('counselor', '==', this.firstname + ' ' + this.lastname)
+    );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       this.noreviews = 1;
-      // doc.data() is never undefined for query doc snapshots
-      // console.log(doc.id, ' =>' , doc.data());
-      const obj = JSON.parse(JSON.stringify(doc.data()));
-      // obj.id = doc.id;
-      //obj.eventId = doc.id;
-    this.fetchedreviews.push(obj);
-    });
 
+      const obj = JSON.parse(JSON.stringify(doc.data()));
+
+      this.fetchedreviews.push(obj);
+    });
   }
-  closeModal(){
+  closeModal() {
     this.modalController.dismiss();
   }
-
-
 
   async onSubmit(form: NgForm) {
     const loading = await this.loadingController.create({
       message: `Submitting review`,
     });
     await loading.present();
-  //More checking regarding the length of review
-  if(form.value.rating==='' || form.value.review==='' || form.value.rating>5){
-  const toast1 = await this.toastController.create({
-  message: 'Please enter a valid rating (0-5) and a review ',
-  duration: 4000,
-  });
-  loading.dismiss();
-  await toast1.present();
-  return;
-  }
-  this.review = {
-    student : this.studentname,
-    counselor: this.firstname + ' '+ this.lastname,
-    review:form.value.review,
-    rating:form.value.rating
-  };
+    //Checking if fields are valid
+    if (
+      form.value.rating === '' ||
+      form.value.review === '' ||
+      form.value.rating > 5 ||
+      form.value.rating < 0
+    ) {
+      const toast1 = await this.toastController.create({
+        message: 'Please enter a valid rating (0-5) and a review ',
+        duration: 4000,
+      });
+      loading.dismiss();
+      await toast1.present();
+      return;
+    }
+    this.review = {
+      student: this.studentname,
+      counselor: this.firstname + ' ' + this.lastname,
+      review: form.value.review,
+      rating: form.value.rating,
+    };
     const review = await this.reviewsService.addReview(this.review);
     await loading.dismiss();
 
@@ -86,5 +94,4 @@ export class ReviewsmodalPage implements OnInit {
     form.reset();
     await toast.present();
   }
-
 }
